1c0efecaee8485eab748616dcdd772f094dab3b6
montyaction <montyaction@hotmail.com>
2023-05-10 19:05:41 +0530

    Update the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./style.css";
 
 const images = [
-  "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-  "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-  "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-  "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-];
+    "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  ];
 
-function App() {
+export default function ImageCarousel() {
   const [current, setCurrent] = useState(0);
 
   function nextSlide() {
@@ -17,27 +17,24 @@ function App() {
   }
 
   function prevSlide() {
-    setCurrent(current === 0 ? images.length - 1 : current - 1);
+    setCurrent(current === 0 ? images.length - 1  : current - 1);
   }
 
-  return (
-    <>
+    return (
       <div>
         <h2>Project 1: Carousel</h2>
-        <div className="carousel slider">
-          <div className="left-arrow" onClick={prevSlide}>⬅</div>
-          <div className="right-arrow" onClick={nextSlide}>⮕</div>
+        <div className="slider">
+            <div className="left-arrow" onClick={prevSlide}>⬅</div>
+            <div className="right-arrow" onClick={nextSlide}>⮕</div>
+            {images.map(
+                (image, index) => current === index && (
+                  <div key={image} className="slide">
+                    <img src={image} className="slide" />
+                  </div>
+                )
+            )}
         </div>
-        {images.map(
-          (image, index) => current === index && (
-            <div key={image} className="slide">
-              <img src={image} alt="images" />
-            </div>
-          )
-        )}
       </div>
-    </>
-  );
-}
-
-export default App
+    );
+  }
+  
